996456e4897dd7dd74e1350022d3c5772addeb41
javierBorjas <cristman11@gmail.com>
2025-09-02 00:49:24 -0300

    creando controlador , modelo y vista de ajuste

---
+++ app/Models/Ajuste.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ajuste extends Model
+{
+    protected $table = 'ajustes';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'sucursal',
+        'direccion',
+        'telefonos',
+        'logo',
+        'logo_auto',
+        'divisa',
+        'correo',
+        'pagina_web',
+    ];
+}
